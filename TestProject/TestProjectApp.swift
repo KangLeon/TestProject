8c24a641153c4ea84fc3a7738c63f168b10c3761
JY <jitengjiao@bytedance.com>
2024-03-11 18:00:02 +0800

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by JY on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
