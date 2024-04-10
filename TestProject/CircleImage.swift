f9425b4e24317216e307240bcfc0493ecd90596a
JY <397879704@qq.com>
2024-04-10 23:06:31 +0800

    new

---
+++ TestProject/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  TestProject
+//
+//  Created by JY on 4/10/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
