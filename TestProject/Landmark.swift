53078a0814e43833b1e416b2bbfcd9f393710e3a
JY <397879704@qq.com>
2024-04-11 23:50:42 +0800

    new

---
+++ TestProject/Landmark.swift
@@ -0,0 +1,18 @@
+//
+//  Landmark.swift
+//  TestProject
+//
+//  Created by JY on 4/11/24.
+//
+
+import SwiftUI
+
+struct Landmark: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Landmark()
+}
