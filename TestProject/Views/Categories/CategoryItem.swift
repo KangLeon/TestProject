58a3fd97a332d4112c3732a6e148f3de2e32d288
JY <397879704@qq.com>
2024-04-14 01:29:37 +0800

    ne w

--- TestProject/Views/Categories/CategoryItem.swift
+++ TestProject/Views/Categories/CategoryItem.swift
@@ -7,12 +7,26 @@
 
 import SwiftUI
 
+
 struct CategoryItem: View {
+    var landmark: Landmark
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            landmark.image
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .foregroundStyle(.primary)
+                .font(.caption)
+        }
     }
 }
 
+
 #Preview {
-    CategoryItem()
+    CategoryItem(landmark: ModelData().landmarks[0])
 }
